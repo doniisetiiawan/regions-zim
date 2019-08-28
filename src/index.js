52f036f787afdc0101f6f3d18a157297cc12b5d8
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-28 13:26:36 +0700

    Responsive with Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.3";
 
 const root = document.getElementById("root");
 if (root) {
