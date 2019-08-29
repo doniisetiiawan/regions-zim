df8db4d0babbe6928280037a56508d632e9d89ea
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-29 12:51:50 +0700

    ...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.splitting";
+import App from "./App.counter";
 
 import { log } from "./logging";
 log.error("myapp:SERVICE:LOGIN", `Attempt`, { user: "FK", pass: "who?" });
