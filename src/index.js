20159818ff62e562ecc0d0052ab187507f695a53
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-29 10:49:41 +0700

    Add react-router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.regions";
+import App from "./App.routing";
 
 const root = document.getElementById("root");
 if (root) {
