559a1ee0f0d44e0067b73e26b0dfaeaf9dec664d
Doni Setiawan <donisetiawan.050@gmail.com>
2019-08-29 11:38:46 +0700

    Add authorization

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.routing";
+import App from "./App.routing.auth";
 
 const root = document.getElementById("root");
 if (root) {
---
+++ src/routingApp/index.js
@@ -0,0 +1,6 @@
+/* @flow */
+
+import { ConnectedLogin } from "./login.connected.js";
+import { AuthRoute } from "./authRoute.connected.js";
+
+export { ConnectedLogin, AuthRoute };
